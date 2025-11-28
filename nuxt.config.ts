380427af7e278300346a063f8d65d3f0f12d1f3b
alvin <dreamboat555@gmail.com>
2025-11-28 22:33:20 +0800

    feat: add default theme css

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,7 +3,7 @@ import tailwindcss from '@tailwindcss/vite'
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   modules: ['@nuxt/eslint'],
-  css: ['~/assets/css/main.css'],
+  css: ['~/assets/css/main.css', '~/assets/css/default-theme.css'],
   ssr: false,
   vite: {
     plugins: [tailwindcss()],
